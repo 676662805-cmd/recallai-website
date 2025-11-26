46b5e4a70aeff6265b263d905165a5ecdb89c063
Your Name <you@example.com>
2025-11-26 05:11:19 -0700

    Make API Vercel-compatible: wrap Express with serverless-http

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
-const app = require('../server.js');
+// Wrap the Express app with serverless-http so Vercel (and other serverless
+// platforms) can invoke it as a function.
+const server = require('../server.js');
+const serverless = require('serverless-http');
 
-module.exports = app;
+module.exports = serverless(server);
