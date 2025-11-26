888dcf39654d89642007f2d0c3d3266372089b99
Your Name <you@example.com>
2025-11-26 05:14:21 -0700

    Revert serverless-http wrapper - Vercel handles Express natively

--- api/index.js
+++ api/index.js
@@ -1,6 +1,3 @@
-// Wrap the Express app with serverless-http so Vercel (and other serverless
-// platforms) can invoke it as a function.
-const server = require('../server.js');
-const serverless = require('serverless-http');
+const app = require('../server.js');
 
-module.exports = serverless(server);
+module.exports = app;
